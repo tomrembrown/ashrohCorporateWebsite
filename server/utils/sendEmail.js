0efbd7c5846c3b3d08cf477306834f1b71c7d7c2
Tom Brown <tom@tomrembrown.com>
2019-07-29 21:38:07 -0400

    Got Email working
    Got email working!

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,36 +1,37 @@
 const nodemailer = require('nodemailer')
 const mg = require('nodemailer-mailgun-transport')
 
+const auth = {
+  auth: {
+    api_key: 'key-01e530e73325f2b75aecd0a9d3346ef4',
+    domain: 'ashroh.com'
+  },
+  proxy: false
+}
+
 const sendEmail = (objectInputData) => {
   console.log('In sendEmail')
   console.log('Name: ' + objectInputData.name)
   console.log('Email: ' + objectInputData.email)
   console.log('Subject: ' + objectInputData.subject)
   console.log('Message: ' + objectInputData.message)
+
+  const nodemailerMailgun = nodemailer.createTransport(mg(auth))
+
+  nodemailerMailgun.sendMail({
+    from: objectInputData.email,
+    to: 'tom@ashroh.com',
+    subject: objectInputData.subject,
+    text: 'Message from ' + objectInputData.name + ' on ashroh.com form\n' + objectInputData.message
+  }, function(err, info) {
+    if (err) {
+      console.log('Error: ' + err)
+    }
+    else {
+      console.log('Response: ' + info)
+    }
+  })
+
 }
-
-// const auth = {
-//   auth: {
-//     api_key: 'key-1234123412341234',
-//     domain: 'mg.ashroh.com'
-//   },
-//   proxy: false
-// }
-
-// const nodemailerMailgun = nodemailer.createTransport(mg(auth))
-
-// nodemailerMailgun.sendMail({
-//   from: inputEmail,
-//   to: 'receiveEmails@ashroh.com',
-//   subject: inputSubject,
-//   message: iniputMessage
-// }, function(err, info) {
-//   if (err) {
-//     console.log('Error: ' + err)
-//   }
-//   else {
-//     console.log('Response: ' + info)
-//   }
-// })
 
 module.exports = sendEmail
